b645d168915195c9023f75810d42cf41bff57b37
Allen Hill <allenofthehills@gmail.com>
2018-03-01 10:26:41 -0500

    Add pretty-printing for `Trial` and `RawTrial`

--- src/trials.jl
+++ src/trials.jl
@@ -31,10 +31,31 @@ struct Trial
     end
 end
 
+Base.show(io::IO, t::Trial) = print(io, t.subject, ", ", t.name, ", ", t.conds)
+
+function Base.show(io::IO, ::MIME"text/plain", t::Trial)
+    println(io, "Subject => ", t.subject)
+    println(io, "Name => ", t.subject)
+    println(io, "Conditions:\n  ", t.conds)
+end
+
 struct RawTrial
     t::Trial
     events::Dict{Symbol,Array}
     data::Matrix
+end
+
+Base.show(io::IO, r::RawTrial) = print(io, '(', r.t, "), ", keys(r.events), ", ",
+                                       typeof(r.data), size(r.data))
+
+function Base.show(io::IO, ::MIME"text/plain", r::RawTrial)
+    show(io, MIME("text/plain"), r.t)
+    println(io, "Events:")
+    for key in keys(r.events)
+        println(io, "  ", key)
+    end
+    println(io, "Data:")
+    println(io, "  ", typeof(r.data), size(r.data))
 end
 
 struct AnalyzedTrial
