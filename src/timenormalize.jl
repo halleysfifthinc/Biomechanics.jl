a667395d032454f896e4242be39aface4c22337b
Allen Hill <allenofthehills@gmail.com>
2018-03-09 12:12:49 -0500

    Extract all Visual3D specific code

--- src/timenormalize.jl
+++ src/timenormalize.jl
@@ -1,34 +1,8 @@
 using Interpolations
 
-export timenormalizestrides,
-       limitcycle
+export timenormalize,
+       avgcycle
 
-
-function timenormalizestrides(trial::Trial, st::Float64, en::T=nothing; kwargs...) where {T<:Void}
-    rt = readtrial(trial, st; kwargs...)
-
-    res = timenormalize(rt.data, rhs)
-
-    return res   
-end
-
-function timenormalizestrides(trial::Trial, st::Float64, en::T; kwargs...) where T
-    rt = readtrial(trial, st; kwargs...)
-    rhs = rt.events[:RHS]
-
-    res = timenormalize(rt.data, rhs)
-    
-    tststr = normtime(rhs[1],rhs[2])
-    pst = findfirst(x -> x >= st*fs, tststr)
-    pen = 0
-    for s in 1:(length(rhs)-1)
-        tststr = normtime(rhs[s],rhs[s+1])
-        pen = findfirst(x -> x >= en*fs, tststr)+(s-1)*fs
-        !iszero(mod(pen,100)) && break
-    end
-    
-    return (res, pst, pen)
-end
 
 function timenormalize(data::AbstractArray, events::AbstractVector,len=100)
     cols = size(data,2)
@@ -73,22 +47,20 @@ function normtime(t1,t2,len=100)
 end
 
 """
-    limitcycle(rootdir::String, sub::Int)
+    avgcycle(data, len=100)
 
-Calculate the ensemble average of the exported data of the subject's steady-state trial
-
-Returns the average and standard deviation time normalized as 100% the length of a stride.
+Calculate the ensemble average and stdev of `data` assuming a normalized length of `len`
 """
-function limitcycle(trial::Trial, numstrides=100, st=30.0; kwargs...)
-    nstr = timenormalizestrides(trial, st; numstrides=numstrides, kwargs...)
-
-    NW = Array{Float64,2}(100, size(nstr,2))
-    vNW = Array{Float64,2}(100, size(nstr,2))
+function avgcycle(data::AbstractArray, len=100)
+    mod(size(data,1),len) == 0 || throw(ArgumentError("length of data must be even"*
+                                                      " multiple of len"))
+    normal = Array{Float64,2}(len, size(nstr,2))
+    vnormal = Array{Float64,2}(len, size(nstr,2))
 
     for dim in 1:size(nstr,2)
-        NW[:,dim] .= [ mean(nstr[i:100:end, dim]) for i in 1:100 ]
-        vNW[:,dim] .= [ std(nstr[i:100:end, dim]) for i in 1:100 ]
+        normal[:,dim] .= [ mean(nstr[i:len:end, dim]) for i in 1:len ]
+        vnormal[:,dim] .= [ std(nstr[i:len:end, dim]) for i in 1:len ]
     end
 
-    return (NW, vNW)
+    return (normal, vnormal)
 end
--- src/trials.jl
+++ src/trials.jl
@@ -1,15 +1,9 @@
 export Trial,
        RawTrial,
        AnalyzedTrial,
-       TrialDescriptor,
-       Visual3D
-
-export readtrial,
-       getsessionorder
+       TrialDescriptor
 
 abstract type TrialDescriptor end
-
-abstract type Visual3D <: TrialDescriptor end
 
 struct Trial{TD}
     subject::Int
@@ -69,63 +63,4 @@ end
 struct AnalyzedTrial
     t::Trial
     results::Dict{Symbol,Any}
-end
-
-function readtrial(trial::Trial{Visual3D}, st::Float64; kwargs...)
-    kwargs = Dict(kwargs)
-    fs = get(kwargs, :fs, 100)
-
-    data = readdlm(trial.path, '\t', Float64; skipstart=5)
-    events = Dict{Symbol,Array}()
-    lhs = round.(Int,filter(!isnan,data[:,2])*fs)
-    rhs = round.(Int,filter(!isnan,data[:,3])*fs)
-
-    prerhs = findfirst(x -> x >= st*fs,rhs)-1
-    lastrhs = haskey(kwargs, :numstrides) ?
-                        (prerhs+kwargs[:numstrides]::Int) : (length(rhs)-1)
-
-    try
-        @assert lastrhs < length(rhs)
-    catch
-        print("\n")
-        throw(ArgumentError("Insufficient number of strides in $(trial.subject),"*
-                            "$(trial.name)"))
-    end
-    
-    data = data[rhs[prerhs]:rhs[lastrhs],get(kwargs, :cols, 4:size(data,2))]
-    events[:RHS] = rhs[prerhs:lastrhs]-rhs[prerhs]+1
-    events[:LHS] = filter!(x -> (rhs[prerhs] <= x <= rhs[lastrhs]), lhs)-rhs[prerhs]+1
-
-    return RawTrial(trial, events, data)
-end
-
-function getsessionorder(session::String)
-    isabspath(session) || throw(ArgumentError("path must be absolute"))
-    isdir(session) || throw(ArgumentError("path must be to a directory"))
-
-    badenfs = Vector{Int}()
-    trialctime = Vector{DateTime}()
-    df = DateFormat("y,m,d,H,M,S")
-
-    trials = readdir(session)
-    filter!(file -> endswith(file,"Trial.enf"), trials)
-
-    for trialnum in eachindex(trials)
-        file = readstring(joinpath(session, trials[trialnum]))
-        m = match(r"CREATIONDATEANDTIME\=(?<timestring>.*)\r", file)
-
-        if m === nothing
-            push!(badenfs, trialnum)
-            continue
-        end
-
-        dt = DateTime(m[:timestring], df)
-        push!(trialctime, dt)
-    end
-
-    deleteat!(trials, badenfs)
-
-    order = sortperm(trialctime)
-
-    return map(x -> basename(x[1:end-10]), trials[order])
 end
