63285e8450763c393fd37d1ddfa1eeeb97ea3106
Allen Hill <allenofthehills@gmail.com>
2018-03-28 12:30:59 -0400

    Performance enhancements
    Change `len` from keyword argument to optional argument; switch to views
    for slicing, and add `@inbounds` where appropriate.

--- src/timenormalize.jl
+++ src/timenormalize.jl
@@ -3,17 +3,16 @@ using Interpolations
 export timenormalize,
        avgcycle
 
-
 function timenormalize(data::AbstractArray, events::AbstractVector,len=100)
     cols = size(data,2)
     res = zeros((length(events)-1)*len, cols)
 
-    fill_normdims!(res, data, events; len=len)
+    fill_normdims!(res, data, events, len)
 
     return res
 end
 
-function fill_normdims!(res::AbstractArray, 
+function fill_normdims!(res::AbstractArray,
                         data::AbstractArray,
                         events::Vector{Int},
                         len=100)
@@ -21,9 +20,9 @@ function fill_normdims!(res::AbstractArray,
         ArgumentError("`res` and `data` must have the same number of columns"))
     for i in 1:size(data,2)
         # Create interpolation object
-        itp = interpolate(@view data[:,i], BSpline(Cubic(Line())), OnGrid())
+        itp = interpolate(@view(data[:,i]), BSpline(Cubic(Line())), OnGrid())
 
-        fill_normstrides!(@view res[:,i], itp, events; len=len)
+        fill_normstrides!(@view(res[:,i]), itp, events, len)
     end
 
     nothing
@@ -35,7 +34,7 @@ function fill_normstrides!(nstr::AbstractArray, str::AbstractArray, events::Vect
 
     # Create time normalized strides
     @inbounds for s in 1:length(events)-1
-        nstr[(1:len)+(s-1)*len] .= str[normtime(events[s],events[s+1],len=len)]
+        nstr[(1:len)+(s-1)*len] .= str[normtime(events[s], events[s+1], len)]
     end
 
     nothing
@@ -54,12 +53,16 @@ Calculate the ensemble average and stdev of `data` assuming a normalized length 
 function avgcycle(data::AbstractArray, len=100)
     mod(size(data,1),len) == 0 || throw(ArgumentError("length of data must be even"*
                                                       " multiple of len"))
-    normal = Array{Float64,2}(len, size(nstr,2))
-    vnormal = Array{Float64,2}(len, size(nstr,2))
 
-    for dim in 1:size(nstr,2)
-        normal[:,dim] .= [ mean(nstr[i:len:end, dim]) for i in 1:len ]
-        vnormal[:,dim] .= [ std(nstr[i:len:end, dim]) for i in 1:len ]
+    N = size(data,1)
+    normal = Array{Float64,2}(len, size(data,2))
+    vnormal = Array{Float64,2}(len, size(data,2))
+
+    @inbounds for dim in 1:size(data,2)
+        @views for i in 1:len
+            normal[i,dim] = mean(data[i:len:N, dim])
+            vnormal[i,dim] = std(data[i:len:N, dim])
+        end
     end
 
     return (normal, vnormal)
