2a806b9b0e8d5028ca5c6074d1f45336d3abaf2c
Allen Hill <allenofthehills@gmail.com>
2018-04-11 19:13:23 -0400

    Update tests for trials.jl

--- test/trials.jl
+++ test/trials.jl
@@ -1,4 +1,8 @@
-abstract type DSTest <: DataSource end
+abstract type DSTest <: AbstractDataSource end
+struct DSTestData <: DSTest
+    events::Dict{Symbol,Any}
+    data::AbstractArray
+end
 
 @testset "Testing trials.jl" begin
     testpath = joinpath(tempname(), "Subject 23", "trialname")
@@ -12,6 +16,19 @@ abstract type DSTest <: DataSource end
         @test_nowarn Trial{DSTest}(23, "trialname", testpath)
         @test_nowarn Trial{DSTest}(23, "trialname", testpath, Dict(:nut => :pecan))
         @test_throws ArgumentError Trial{DSTest}(23, "trialname", testpath[4:end])
+    end
+
+    t = Trial{DSTest}(testpath)
+
+    @testset "Segment tests" begin
+        @test_nowarn d = DSTestData(Dict{Symbol,Any}(),rand(10,10))
+        @test_nowarn Segment(t,d)
+    end
+
+    d = DSTestData(Dict{Symbol,Any}(),rand(10,10))
+
+    @testset "AnalyzedSegment tests" begin
+        @test_nowarn as = AnalyzedSegment(Segment(t,d), Dict{Symbol,Any}())
     end
 
     @test_throws MethodError readtrial(Trial{DSTest}(testpath))
