25581669bddf6f1bf66e48f7ffcc2b8b7b2f07b7
Allen Hill <allenofthehills@gmail.com>
2018-04-11 19:00:00 -0400

    Fairly major (conceptually) overhaul
    There are a large number of changes, all organized around a
    solidification of the terminology and generalization for various
    usecases.
    
    `AbstractDataSource` needs to be implemented for each new datasource (eg
    a CSV, an HDF5 file, etc.). A `readtrial` method should be added for
    each datasource type.
    
    Additionally, a "base" segment type, which is a subtype of the custom
    datasource type should be added. This base segment type is what should
    be returned by the `readtrial` method. It generally will just include
    the data from the whole trial and any additional information (eg events)
    which are considered part of the trial data (ie and not part of the
    `Trial` type describing the trial as a whole.

--- src/trials.jl
+++ src/trials.jl
@@ -1,31 +1,34 @@
 export Trial,
-       RawTrial,
-       AnalyzedTrial,
-       DataSource
+       Segment,
+       AnalyzedSegment,
+       AbstractDataSource
 
 export readtrial
 
 """
-    DataSource
+    AbstractDataSource
 
 In recognition that data sources vary, and the mechanism of reading trials will differ 
-between data sources, implement a DataSource for your data.
+between data sources, implement a subtype of AbstractDataSource for your data.
 """
-abstract type DataSource end
+abstract type AbstractDataSource end
 
 """
     Trial{DS}
 
-A trial should describe the particulars of the data source. Trials are parameterized for
+A `Trial` describes the referenced trial. Trials are parameterized for
 different datasources to allow for dispatching by the Trial parameter.
 """
-struct Trial{DS<:DataSource}
+struct Trial{DS<:AbstractDataSource}
     "The subject identifier"
     subject::Integer
+
     "The trial name"
     name::String
+
     "The absolute path to the file containing the trial data"
     path::String
+
     "The specific trial conditions; if unneeded, this can be empty"
     conds::Dict{Symbol,Symbol}
 
@@ -36,7 +39,7 @@ struct Trial{DS<:DataSource}
     of conditions.
     """
     function Trial{DS}(path::String,
-                   subbase::String="Subject") where DS <: DataSource
+                   subbase::String="Subject") where DS <: AbstractDataSource
         isabspath(path) || throw(ArgumentError("path must be absolute"))
 
         name = splitext(basename(path))[1]
@@ -52,7 +55,7 @@ struct Trial{DS<:DataSource}
 
     Create a completely specified trial.
     """
-    function Trial{DS}(s,n,p,conds=Dict{Symbol,Symbol}()) where DS <: DataSource
+    function Trial{DS}(s,n,p,conds=Dict{Symbol,Symbol}()) where DS <: AbstractDataSource
         isabspath(p) || throw(ArgumentError("path must be absolute"))
 
         return new(s,n,p,conds)
@@ -68,45 +71,43 @@ function Base.show(io::IO, ::MIME"text/plain", t::Trial{DS}) where DS
     println(io, "Conditions:\n  ", t.conds)
 end
 
-"""
-    readtrial(::Trial{DS})
-
-Throws a MethodError. `readtrial` methods must be defined for a particular DataSource.
-"""
 function readtrial(t::Trial{DS}) where DS
     throw(MethodError(readtrial, (t,)))
 end
 
 """
-    RawTrial
+    Segment
 
-A RawTrial wraps the base Trial, and includes any data and events which may be of interest.
+A `Segment` is a container which includes all of, or a segment of the data from a particular `Trial`. 
 """
-struct RawTrial
-    t::Trial
-    events::Dict{Symbol,Array}
-    data::Matrix
+struct Segment{DS<:AbstractDataSource,D<:DS}
+    trial::Trial{DS}
+    data::D
 end
 
-Base.show(io::IO, r::RawTrial) = print(io, '(', r.t, "), ", keys(r.events), ", ",
-                                       typeof(r.data), size(r.data))
+Base.show(io::IO, s::Segment{DS,D}) where {DS,D} = print(io, "Segment{",D,"}(",s.trial,")")
 
-function Base.show(io::IO, ::MIME"text/plain", r::RawTrial)
-    show(io, MIME("text/plain"), r.t)
-    println(io, "Events:")
-    for key in keys(r.events)
-        println(io, "  ", key)
-    end
-    println(io, "Data:")
-    println(io, "  ", typeof(r.data), size(r.data))
+function Base.show(io::IO, ::MIME"text/plain", s::Segment{DS,D}) where {DS,D}
+    println(io, "Segment{",D,"}")
+    show(io, MIME("text/plain"), s.trial)
+    show(io, MIME("text/plain"), s.data)
 end
 
 """
-    AnalyzedTrial
+    AnalyzedSegment
 
-Contains the results of any analysis/analyses performed on the trial.
+Contains the results of any analysis/analyses performed on the trial segment.
 """
-struct AnalyzedTrial
-    t::Trial
+struct AnalyzedSegment{DS<:AbstractDataSource,D<:DS}
+    s::Segment{DS,D}
     results::Dict{Symbol,Any}
 end
+
+Base.show(io::IO, as::AnalyzedSegment) = print(io, "AnalyzedSegment(",as.s,",", as.results,")")
+
+function Base.show(io::IO, ::MIME"text/plain", as::AnalyzedSegment{DS,D}) where {DS, D}
+    println(io, "AnalyzedSegment{",D,"}")
+    show(io, MIME("text/plain"), as.s)
+    show(io, MIME("text/plain"), as.results)
+end
+
