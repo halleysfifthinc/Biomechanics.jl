1a740170ad41056afffca927f8388a0b365f7e55
Allen Hill <allenofthehills@gmail.com>
2018-05-16 15:11:20 -0400

    Add monodec/monoinc functions

--- src/helperfuncs.jl
+++ src/helperfuncs.jl
@@ -5,7 +5,9 @@ export calcresiduals,
        demean,
        demean!,
        detrend,
-       detrend!
+       detrend!,
+       monoinc,
+       monodec
 """
     demean(x)
 
@@ -45,6 +47,35 @@ function detrend!(y::AbstractVector)
 end
 
 """
+    monodec(x)
+
+Check if a vector is monotonically decreasing
+"""
+function monodec(x::AbstractVector)
+    for i in eachindex(x)[1:end-1]
+        if x[i+1] > x[i]
+            return false
+        end
+    end
+    return true
+end
+
+"""
+    monoinc(x)
+
+Check if a vector is monotonically increasing
+"""
+function monoinc(x::AbstractVector)
+    for i in eachindex(x)[1:end-1]
+        if x[i+1] < x[i]
+            return false
+        end
+    end
+    return true
+end
+
+
+"""
     calcresiduals(data::Vector{Float64},
                     fs::Int64,
                     fc::Vector{Float64}=collect(.05:.05:25.0))::Vector{Float64}
@@ -66,10 +97,10 @@ function calcresiduals(data::Vector{Float64},
     len = length(fc)
     R = zeros(Float64,len)
     N = length(data)
-    @fastmath @inbounds @simd for j in 1:length(fc)
+    for j in 1:length(fc)
         responsetype = Lowpass(fc[j]*1.247; fs=fs)
         thefilter = digitalfilter(responsetype, designmethod)
-        R[j] = sqrt((1/N)*sum(data-filtfilt(thefilter,data))^2)
+        R[j] = sqrt((1/N)*sum((data.-filtfilt(thefilter,data)).^2))
     end
     return R
 end
@@ -91,13 +122,12 @@ function optfc(R::Vector{Float64},
                          lb::Float64=15.0)::Tuple{Float64,Float64,Float64}
     q = findin(fc,lb)
     c = q[1]
-    len = length(fc)
-    a::Float64,b = linreg(fc[c:end],R[c:end])
-    @fastmath @inbounds @simd for i in 1:len
-        if ~any(x::Float64 -> (x > a::Float64), R[i:end])
-            return (fc[i], a, b)
-        end
+    a,b = linreg(fc[c:end],R[c:end])
+    i = findfirst(x -> x <= a, R)
+    if i == 0
+        return (0.0, a, b)
+    else
+        return (fc[i], a, b)
     end
-    return (0.0, a, b)
 end
 
